7d6de7e1ba4ef730e7b3be00cc9aa2ac2aafa8e5
colons <colons@colons.co>
2020-02-01 09:30:19 +0000

    Make the command-line version use the stuff we've written.

--- game.py
+++ game.py
@@ -1,15 +1,43 @@
-from random import choice
+import os
+from random import choice, sample
+
+
+CATEGORIES_DIR = os.path.join(os.path.dirname(__file__), 'categories')
+
+
+def get_categories_shortlist():
+    return sample(list(CATEGORIES), 6)
+
+
+def _load_category(filename):
+    category_name, _ = os.path.splitext(filename)
+    releases = []
+    with open(os.path.join(CATEGORIES_DIR, filename)) as cf:
+        for line in cf.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith('>'):
+                releases[-1]['comments'].append(line[1:].strip())
+            else:
+                releases.append({'name': line, 'comments': []})
+
+    return category_name, releases
+
+
+CATEGORIES = {name: category for name, category in (
+    _load_category(fn) for fn in os.listdir(CATEGORIES_DIR)
+)}
 
 
 if __name__ == '__main__':
-    product = choice([
-        'spiders',
-        'Blåhaj',
-        'Fallout 76',
-        'Coca-Cola',
-        'Wooloo',
-    ])
+    shortlist = get_categories_shortlist()
+    for i, cat in enumerate(shortlist):
+        print(f'[{i + 1}] {cat}')
 
+    category = shortlist[int(input('pick a category!\n> ')) - 1]
+
+    product = choice(CATEGORIES[category])['name']
     version = 'version {}'.format(choice([
         '0.0.1',
         '1.0 alpha 3',
@@ -20,5 +48,5 @@ if __name__ == '__main__':
     ]))
 
     print(
-        f'New relase: {product}, version {version}!\n\nChanges include:\n\n>'
+        f'New release: {product}, version {version}!\n\nChanges include:\n\n>'
     )
